c73fbcb65c4371015b41ee8fa2fb8419adcc80f0
Luka Lazarević <lukalazarevic00@gmail.com>
2023-06-14 22:00:18 +0200

    ormar radi mapa dodata

--- app/src/main/java/com/luka/mosis/MainFragment.kt
+++ app/src/main/java/com/luka/mosis/MainFragment.kt
@@ -1,24 +1,43 @@
 package com.luka.mosis
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Canvas
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import androidx.preference.PreferenceManager
+import coil.load
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.luka.mosis.databinding.FragmentMainBinding
+import org.osmdroid.config.Configuration
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.CustomZoomButtonsController
+import org.osmdroid.views.MapView
+import org.osmdroid.views.Projection
+import org.osmdroid.views.overlay.compass.CompassOverlay
+import org.osmdroid.views.overlay.gestures.RotationGestureOverlay
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
+
 
 class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
     private val user : User by viewModels()
+    lateinit var mapa : MapView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -33,19 +52,80 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.toolbar.setupWithNavController(findNavController(), AppBarConfiguration(setOf(R.id.mainFragment)))
+
+        binding.toolbar.setupWithNavController(findNavController(), AppBarConfiguration(setOf(R.id.mainFragment),binding.drawerLayout))
+        binding.navView.inflateMenu(R.menu.menu_drawer)
+        binding.navView.inflateHeaderView(R.layout.menu_header)
+        binding.navView.getHeaderView(0).findViewById<TextView>(R.id.menu_header_top).text = Firebase.auth.currentUser?.displayName
+        binding.navView.getHeaderView(0).findViewById<TextView>(R.id.menu_header_bottom).text = Firebase.auth.currentUser?.email
+        binding.navView.getHeaderView(0).findViewById<ImageView>(R.id.menu_header_image).load(Firebase.auth.currentUser?.photoUrl)
+        binding.navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.logout_item -> {
+                    Firebase.auth.signOut()
+                    user.user = Firebase.auth.currentUser
+                    findNavController().navigate(R.id.action_mainFragment_to_loginFragment)
+                }
+                else -> {
+                }
+            }
+            true
+        }
+        val ctx: Context? = requireActivity().applicationContext
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx!!))
+        mapa = binding.mapa
+        mapa.setMultiTouchControls(true)
+        if(ActivityCompat.checkSelfPermission(requireActivity(),android.Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(requireActivity(),android.Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+            registerForActivityResult(ActivityResultContracts.RequestPermission()){
+                    isGranted:Boolean->
+                if(isGranted){
+                    setMyLocationOverlay()
+                }
+            }.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+        }else{
+            setMyLocationOverlay()
+        }
+        mapa.controller.setZoom(15.0)
+        mapa.controller.setCenter(GeoPoint(43.3209, 21.8958))
+        //mapa.zoomController.setVisibility(CustomZoomButtonsController.Visibility.NEVER)
+
+        val rotacijaOverlay = RotationGestureOverlay(mapa)
+        rotacijaOverlay.isEnabled = true
+        mapa.overlays.add(rotacijaOverlay)
+        val compassOverlay = object: CompassOverlay(context, mapa){
+            override fun draw(c: Canvas?, pProjection: Projection?) {
+                drawCompass(c, -mapa.mapOrientation, pProjection?.screenRect)
+            }
+
+            override fun onSingleTapUp(e: MotionEvent?, mapView: MapView?): Boolean {
+                mapa.setMapOrientation(0.0f,false);
+                return true
+            }
+        }
+        compassOverlay.enableCompass()
+        mapa.overlays.add(compassOverlay)
+    }
+    private fun setMyLocationOverlay(){
+        val myLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(requireActivity()),mapa)
+        myLocationOverlay.enableMyLocation()
+        mapa.overlays.add(myLocationOverlay)
     }
     override fun onStart() {
         super.onStart()
-        Log.d("GA","MAJok")
-        binding.logoutButton.setOnClickListener{
-            Firebase.auth.signOut()
-            user.user = Firebase.auth.currentUser
-            findNavController().navigate(R.id.action_mainFragment_to_loginFragment)
-        }
     }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapa.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapa.onPause()
     }
 }
--- app/src/main/java/com/luka/mosis/RegisterFragment.kt
+++ app/src/main/java/com/luka/mosis/RegisterFragment.kt
@@ -1,5 +1,7 @@
 package com.luka.mosis
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
@@ -8,15 +10,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks.await
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.ktx.userProfileChangeRequest
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.app
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.luka.mosis.databinding.FragmentRegisterBinding
+import java.io.File
+import java.net.URI
+
 
 class RegisterFragment : Fragment() {
 
@@ -24,10 +38,14 @@ class RegisterFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var storage: StorageReference
     private val user: User by viewModels()
+    private lateinit var pickMedia: ActivityResultLauncher<PickVisualMediaRequest>
+    private var imageUri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         auth = Firebase.auth
+        storage = FirebaseStorage.getInstance(Firebase.app).reference
     }
 
     override fun onCreateView(
@@ -37,6 +55,15 @@ class RegisterFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentRegisterBinding.inflate(inflater, container, false)
         binding.toolbar.setupWithNavController(findNavController())
+        pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            if (uri != null) {
+                imageUri = uri
+                binding.registerImage.setImageURI(uri)
+                Log.d("PhotoPicker", "Selected URI: $uri")
+            } else {
+                Log.d("PhotoPicker", "No media selected")
+            }
+        }
         return binding.root
     }
 
@@ -54,6 +81,7 @@ class RegisterFragment : Fragment() {
         val username = binding.registerUsernameEdittext
         val phone = binding.registerPhoneEdittext
         var validPhone = Patterns.PHONE.matcher(phone.text).matches()
+        val image = binding.registerImage
         email.doOnTextChanged { text, _, _, _ ->
             validEmail = !TextUtils.isEmpty(text) && Patterns.EMAIL_ADDRESS.matcher(text!!).matches()
             registerButton.isEnabled = validPassword && validEmail && validPhone && !username.text.isNullOrEmpty()
@@ -74,13 +102,42 @@ class RegisterFragment : Fragment() {
             registerButton.isEnabled = validPassword && validEmail && validPhone && !username.text.isNullOrEmpty()
         }
         registerButton.isEnabled = validPassword && validEmail && validPhone && !username.text.isNullOrEmpty()
+
+        image.setOnClickListener {
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
         registerButton.setOnClickListener {
             auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
             .addOnCompleteListener(requireActivity()) { task ->
+                val taskovi = mutableListOf<Any>()
                 if (task.isSuccessful) {
-                    Toast.makeText(this.context, "Decko ti si registrovan upravo", Toast.LENGTH_LONG).show()
-                    user.user = auth.currentUser
-                    findNavController().navigate(R.id.action_registerFragment_to_mainFragment)
+                    if(imageUri!=null){
+                        val firebaseImages = storage.child("images/${auth.currentUser?.uid}")
+                        firebaseImages.putFile(imageUri!!).continueWithTask { task ->
+                            if (task.isSuccessful) {
+                                firebaseImages.downloadUrl
+                            }else{
+                                task.exception?.let {
+                                    throw it
+                                }
+                            }
+                        }.addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                auth.currentUser?.updateProfile(userProfileChangeRequest {
+                                    photoUri = task.result
+                                    displayName = username.text.toString()
+                                })?.addOnCompleteListener {
+                                    findNavController().navigate(R.id.action_registerFragment_to_mainFragment)
+                                }
+                            }
+                        }
+                    }else{
+                        auth.currentUser?.updateProfile(userProfileChangeRequest {
+                            displayName = username.text.toString()
+                        })?.addOnCompleteListener {
+                            findNavController().navigate(R.id.action_registerFragment_to_mainFragment)
+                        }
+                    }
                 } else {
                     Log.w("TAG", "signInWithEmail:failure", task.exception)
                     Toast.makeText(this.context, "Dalje neces moci", Toast.LENGTH_LONG).show()
